24fcd93277b6b8eda024af93337a91be4f0446c8
rachaouldbabaali <rachaould@gmail.com>
2023-06-07 15:25:15 +0100

    Add react testing library

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Calculator from '../components/Calculator';
 
 describe('Calculator', () => {
   it('renders the component', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the component', () => {
+    const { getByText } = render(<Calculator result={{}} setResult={() => {}} />);
+    expect(getByText('Let\'s do some math!')).toBeInTheDocument();
   });
 });
